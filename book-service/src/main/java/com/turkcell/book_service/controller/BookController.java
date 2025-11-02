11cd2dfc7c88aacae5196772ce943697e4dbf763
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2025-11-02 16:50:50 +0300

    add outbox patter implementation
    Co-Authored-By: Ardach <43939501+Ardach@users.noreply.github.com>
    Co-Authored-By: Alişan <120260926+alisanakbaba@users.noreply.github.com>

--- book-service/src/main/java/com/turkcell/book_service/controller/BookController.java
+++ book-service/src/main/java/com/turkcell/book_service/controller/BookController.java
@@ -13,6 +13,13 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.book_service.messaging.outbox.OutboxMessage;
+import com.turkcell.book_service.messaging.outbox.OutboxRepository;
+
+import java.util.UUID;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -25,27 +32,47 @@ import java.util.UUID;
 public class BookController {
 
     private final BookApplicationService bookApplicationService;
-    private final StreamBridge streamBridge;
+    // private final StreamBridge streamBridge;
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
 
-    public BookController(BookApplicationService bookApplicationService, StreamBridge streamBridge) {
+    public BookController(BookApplicationService bookApplicationService, OutboxRepository outboxRepository,
+            ObjectMapper objectMapper) {
         this.bookApplicationService = bookApplicationService;
-        this.streamBridge = streamBridge;
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
+        // this.streamBridge = streamBridge;
     }
 
     record KafkaTestRequest(String kafkaTest) {
     }
 
-    record KafkaTestEvent(String kafkaTest) {
+    public record KafkaTestEvent(String kafkaTest) {
     }
 
     @PostMapping("/kafka-test")
-    public String kafkaTest(@RequestBody KafkaTestRequest request) {
+    public String kafkaTest(@RequestBody KafkaTestRequest request) throws JsonProcessingException {
+
         KafkaTestEvent event = new KafkaTestEvent(request.kafkaTest);
-        Message<KafkaTestEvent> message = MessageBuilder.withPayload(event).build();
-        streamBridge.send("kafkaTest-out-0", message);
 
-        System.out.println("Kafka test event gönderildi: " + request.kafkaTest);
+        OutboxMessage outboxMessage = new OutboxMessage();
+        outboxMessage.setAggregateId(UUID.randomUUID()); // normalde db'de oluşan order'ın idsi
+        outboxMessage.setAggregateType("Order");
+        outboxMessage.setEventId(UUID.randomUUID());
+        outboxMessage.setEventType("OrderCreatedEvent");
+        outboxMessage.setPayloadJson(objectMapper.writeValueAsString(event));
+        outboxRepository.save(outboxMessage);
 
+        // Message<KafkaTestEvent> message = MessageBuilder.withPayload(event).build();
+        // try {
+        // boolean isSent = streamBridge.send("kafkaTest-out-0", message);
+        // if (!isSent)
+        // System.out.println("Mesaj gönderilemedi");
+        // } catch (Exception e) {
+        // System.out.println("Mesaj gönderilemedi");
+        // }
+
+        System.out.println("Kafka test event outbox'a kayıt edildi: " + request.kafkaTest);
         return request.kafkaTest;
     }
 
---
+++ book-service/src/main/java/com/turkcell/book_service/messaging/outbox/OutboxStatus.java
@@ -0,0 +1,7 @@
+package com.turkcell.book_service.messaging.outbox;
+
+public enum OutboxStatus {
+	PENDING,
+	SENT,
+	FAILED
+}
